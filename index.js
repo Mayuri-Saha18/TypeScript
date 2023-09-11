613ef691122bfc9888961bbd6384bf5e5c938bc3
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-09-11 17:52:54 +0530

    add

---
+++ index.js
@@ -0,0 +1,13 @@
+var x = 5;
+var y = 10;
+y += 4;
+console.log(x + y);
+// intro
+var x = "5";
+var y = 10;
+y += 4;
+console.log(x, y);
+function sum(a, b) {
+    return a + b;
+}
+sum(3, 4);
---
+++ index.ts
@@ -0,0 +1,16 @@
+const x=5
+let y=10
+y+=4
+console.log(x+y)
+
+// intro
+const x:string="5";
+let y=10;
+y+=4;
+console.log(x,y);
+
+
+function sum(a:number,b:number){
+    return a+b;
+}
+sum(3,4);
